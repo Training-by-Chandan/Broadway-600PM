6573e2d134dd2a9678cd0b8c7462ba4a8b02f182
Chandan Gupta <chandan.bhagat@@outlook.com>
2021-05-19 19:22:49 +0545

    String Functions, switch case , math functions explained

--- Broadway600PM/Program.cs
+++ Broadway600PM/Program.cs
@@ -10,6 +10,126 @@ namespace Broadway600PM
     {
         static void Main()
         {
+            //InitialCode();
+
+            //MathFunctions();
+
+            //StringFunctions();
+
+            SwitchCaseExample();
+
+
+
+            Console.ReadLine();
+            
+        }
+
+        static void SwitchCaseExample()
+        {
+            Console.WriteLine("Enter the number of day");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                default:
+                    break;
+            }
+
+            switch(number)
+            {
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("Weeekdays");
+                    break;
+                case 1:
+                case 7:
+                    Console.WriteLine("Weekends");
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        static void StringFunctions()
+        {
+            Console.WriteLine("Enter First Name");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Enter Middle Name");
+            var middlename = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            var lastName = Console.ReadLine();
+            string fullname = "";
+            //var fullname= firstName + " " + middlename + " " + lastName;
+            //var fullname1 = string.Concat(firstName, middlename, lastName);
+            //var fullname2 = string.Format("{0} {1} {2}", firstName, middlename, lastName);
+            //var fullname3 = $"{firstName} {middlename} {lastName}";
+            //var replaced = fullname.Replace(" ", "   ");
+            //var length = fullname.Length;
+
+            if (string.IsNullOrWhiteSpace(middlename))
+                fullname = $"{firstName} {lastName}";
+            else
+                fullname = $"{firstName} {middlename} {lastName}";
+            // (condition)?True statement : false statement;
+            
+            fullname = string.IsNullOrWhiteSpace(middlename) ? $"{firstName} {lastName}" : $"{firstName} {middlename} {lastName}";
+
+           // StringBuilder sb = new StringBuilder();
+           // sb.Append("My name is Abhash.\n");
+           // sb.Append("My name is Biraj.\n");
+           // sb.Append("My name is Dipendra.\n");
+           // sb.Append("My name is Saramsh.\n");
+           //sb.Append("My name is Nawanit.\n");
+           // string output = sb.ToString();
+
+            var finalstr1 = "My Name is " + fullname;
+            Console.WriteLine(finalstr1);
+
+
+
+          
+            
+        }
+
+        static void MathFunctions() 
+        {
+            Console.WriteLine("Enter the base");
+            var baseNum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Power");
+            var powNum = Convert.ToDouble(Console.ReadLine());
+            var res = Math.Pow(baseNum, powNum);
+            Console.WriteLine(res);
+        }
+
+        static void InitialCode()
+        {
             //ctrl + k,c for comment
             //ctrl + k, u for uncomment
 
@@ -21,7 +141,7 @@ namespace Broadway600PM
             Console.Read();
             var t = Console.ReadLine();
 
-            
+
             //implicit casting
             char c = '1';
             int i = c;
@@ -41,9 +161,6 @@ namespace Broadway600PM
             string str = number.ToString();
 
             var res = i + numInt;
-
-            Console.ReadLine();
-            
         }
     }
 }
