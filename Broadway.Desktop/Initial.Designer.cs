6a62b1ae19f70de5741a5c6de9241defd9967f3f
Chandan Gupta <chandan.bhagat@@outlook.com>
2021-06-16 19:27:51 +0545

    updated the project with database conenction and populated the data.

---
+++ Broadway.Desktop/Initial.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Broadway.Desktop
+{
+    partial class Initial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(71, 287);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1105, 267);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(332, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(286, 38);
+            this.txtName.TabIndex = 2;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(683, 66);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(96, 38);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(687, 151);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(130, 59);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Initial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 615);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Initial";
+            this.Text = "Initial";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
---
+++ Broadway.Desktop/Initial.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Broadway.Desktop
+{
+    public partial class Initial : Form
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+        public Initial()
+        {
+            InitializeComponent();
+            LoadDataV2();
+        }
+
+        private void LoadData()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            string query = "select * from Student";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                var result = cmd.ExecuteReader();
+                List<Student> list = new List<Student>();
+                while (result.Read())
+                {
+                    var student = new Student();
+                    student.Id = result.GetFieldValue<int>(0);
+                    student.Name = result.GetFieldValue<string>(1);
+                    list.Add(student);
+                }
+
+                dataGridView1.DataSource = list;
+                dataGridView1.Refresh();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void LoadDataV2()
+        {
+            var ds = new DataSet();
+            SqlConnection con = new SqlConnection(connectionString);
+            string query = "select * from Student";
+
+            SqlDataAdapter adapt = new SqlDataAdapter(query, con);
+            try
+            {
+                adapt.Fill(ds);
+
+                dataGridView1.DataSource = ds.Tables[0];
+                dataGridView1.Refresh();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            InsertData();
+        }
+
+        private void InsertData()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            string query = "insert into student (name) values ('" + txtName.Text + "')";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                var result = cmd.ExecuteNonQuery();
+
+                txtName.Text = "";
+                LoadDataV2();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void UpdateData()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            string query = "update student set name= '" + txtName.Text + "' where id=1";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                var result = cmd.ExecuteNonQuery();
+
+                txtName.Text = "";
+                LoadDataV2();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateData();
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
